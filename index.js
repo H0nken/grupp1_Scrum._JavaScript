23fd0a4355e98d4d6e5763e1dfa4a8b5c02a8d06
JonasHolmkvist <jonas_holmkvist@hotmail.se>
2020-12-04 16:34:26 +0100

    added function for upload

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//test()
+const album2 = document.getElementById("myFileInput2");
+
+//file-input
+document.querySelector("#myFileInput").addEventListener("change", function(){
+    //this will convert file to dataURL
+    const reader = new FileReader();
+    reader.addEventListener("load", () =>{
+        localStorage.setItem(localStorage.length, reader.result);
+    });
+
+    //This looking for a loaded file and store it
+    reader.readAsDataURL(this.files[0]);
+ 
+ });
+
+    //image-preweiv
+     document.addEventListener("DOMContentLoaded", () =>{
+         let photos = document.getElementById("photos1");
+         for(let i=0;i<localStorage.length;i++){
+            console.log("Bild" +localStorage.getItem(i));
+         }
+         //create new elem-section in HTML and save the image there
+         for(let i=0;i<localStorage.length;i++){
+            let elem = document.createElement("img");
+            elem.setAttribute("src",localStorage.getItem(i));
+            photos.appendChild(elem);
+
+         }
+        
+         
+ 
+  });
+
